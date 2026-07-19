2ba1abbe76885a3184aac7f31b0e9a3f16132494
KennFatt <kennfatt@gmail.com>
2026-07-20 02:11:12 +0700

    fix(version): show the version via `ReadBuildInfo()`

--- cmd/manly/command_version.go
+++ cmd/manly/command_version.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 // version is overridden at link time for release builds.
 var version = "dev"
@@ -8,6 +11,19 @@ var version = "dev"
 type VersionCommand struct{}
 
 func (command VersionCommand) Run(_ *appContext) error {
-	fmt.Printf("manly %s\n", version)
+	fmt.Printf("manly %s\n", executableVersion())
 	return nil
+}
+
+func executableVersion() string {
+	if version != "dev" {
+		return version
+	}
+
+	buildInfo, ok := debug.ReadBuildInfo()
+	if ok && buildInfo.Main.Version != "" && buildInfo.Main.Version != "(devel)" {
+		return buildInfo.Main.Version
+	}
+
+	return version
 }
